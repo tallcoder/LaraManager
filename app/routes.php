c241c19ab6083a2814e95f5654c785be9eb53699
mike <mike@internetconnection.net>
2013-12-17 22:57:27 -0500

    updated routes

--- app/routes.php
+++ app/routes.php
@@ -26,3 +26,21 @@ Route::resource('comments', 'CommentsController', array('only' => array('store')
 Route::resource('projects.tasklists', 'TasklistsController');
 Route::resource('projects.tasks', 'TasksController');
 
+//delete handling -- delete should actually be handled resourcefully
+Route::get('projects/{projects}/delete', array('as' => 'projects.delete', 'uses' => 'ProjectsController@delete'));
+Route::post('projects/{projects}/destroy', array('as' => 'projects.destroy', 'uses' => 'ProjectsController@destroy'));
+Route::get('projects/{projects}/tasks/{task}/delete', array('as' => 'projects.tasks.delete',
+                                                     'uses' => 'TasksController@delete'));
+Route::post('projects/{projects}/tasks/{tasks}/destroy', array('as' => 'projects.tasks.destroy',
+                                                    'uses' => 'TasksController@destroy'));
+Route::get('projects/{projects}/tasklists/{tasklists}/delete', array('as' => 'projects.tasklists.delete',
+                                                     'uses' => 'TasklistsController@delete'));
+Route::post('projects/{projects}/tasklists/{tasklists}/destroy', array('as' => 'projects.tasklists.destroy',
+                                                'uses' => 'TasklistsController@destroy'));
+Route::get('comments/{comments}/delete', array('as' => 'comments.delete', 'uses' => 'CommentsController@delete'));
+Route::post('comments/{comments}/destroy', array('as' => 'comments.destroy', 'uses' => 'CommentsController@destroy'));
+Route::get('users/{users}/delete', array('as' => 'users.delete', 'uses' => 'UsersController@delete'));
+Route::post('users/{users}/destroy', array('as' => 'users.destroy', 'uses' => 'UsersController@destroy'));
+
+
+
